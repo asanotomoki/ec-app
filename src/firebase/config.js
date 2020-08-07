6c8a355f7256717b714c05cb2937c0efa8246afc
asanotomoki <mirai20190820@gmail.com>
2020-08-08 05:57:05 +0900

    File organization clone assetsFolder,add firebasefile

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export  const firebaseConfig = {
+    apiKey: "AIzaSyBTseCJ7IE97h-mC3Xh95YHid0PIBJ0qzM",
+    authDomain: "ec-app-9d64f.firebaseapp.com",
+    databaseURL: "https://ec-app-9d64f.firebaseio.com",
+    projectId: "ec-app-9d64f",
+    storageBucket: "ec-app-9d64f.appspot.com",
+    messagingSenderId: "999605308118",
+    appId: "1:999605308118:web:3084e3756f913451559777",
+    measurementId: "G-D954Y8ZFGC"
+  };
